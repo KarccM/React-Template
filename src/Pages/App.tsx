f232d784d3607545075b22989d01906fb51f6a77
Kareem <kareem@gmail.com>
2022-10-02 11:33:05 +0300

    template

---
+++ src/Pages/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+            <p className='bg-indigo-400'>
+                template
+            </p>
+        </div>
+    )
+}
+
+export default App
